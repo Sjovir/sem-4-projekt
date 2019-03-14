c608d005815bbab5ca4561a4904691ebbd2e10e6
AndersBensen <anders_bensen@hotmail.com>
2019-03-14 11:00:02 +0100

    Test classes
    In each module

---
+++ gms/src/main/java/org/grp2/gms/Test.java
@@ -0,0 +1,7 @@
+package org.grp2.gms;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("HELLO, I AM GMS");
+    }
+}
---
+++ gnode/src/main/java/org/grp2/gnode/Test.java
@@ -0,0 +1,8 @@
+package org.grp2.gnode;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.printf("HELLO, I AM GNODE");
+    }
+}
