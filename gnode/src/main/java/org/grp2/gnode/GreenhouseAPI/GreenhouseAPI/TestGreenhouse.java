c4c5abf01fbfd6e6316bb4a13c25eaff290e4dbf
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2019-03-20 11:09:03 +0100

    added test statements

--- gnode/src/main/java/org/grp2/gnode/GreenhouseAPI/GreenhouseAPI/TestGreenhouse.java
+++ gnode/src/main/java/org/grp2/gnode/GreenhouseAPI/GreenhouseAPI/TestGreenhouse.java
@@ -18,8 +18,7 @@ import java.util.BitSet;
  */
 public class TestGreenhouse 
 {
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) throws InterruptedException {
         //PLCConnection con = new UDPConnection(1025, "localhost");
         PLCConnection con = new UDPConnection(5000, "192.168.0.40");
         //PLCConnection con = new SerialConnection("COM4");
@@ -30,13 +29,19 @@ public class TestGreenhouse
 
         api.SetBlueLight(0);
 
-        api.SetFanSpeed(2);
+        api.SetFanSpeed(1);
 
-        //api.SetTemperature(273 + 25);
+        api.SetTemperature(273 + 25);
         //api.SetFanSpeed(1);
-        //double outdoorTemperature; 
-        //while (true)
-        //   outdoorTemperature = api.ReadTemp2();
+        double outdoorTemperature;
+        while (true) {
+            Thread.sleep(3000);
+            outdoorTemperature = api.ReadTemp2();
+            System.out.println(outdoorTemperature);
+            System.out.println(api.ReadWaterLevel());
+            System.out.println(api.ReadTemp1());
+            System.out.println(api.ReadMoist());
+        }
         
        
         
