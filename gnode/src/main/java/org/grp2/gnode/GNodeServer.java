043e3522cec838dff3c2732772f7d80179eabdad
Unknown <erik@hagebjerg.dk>
2019-04-04 10:22:37 +0200

    Remove changes to main method

--- gnode/src/main/java/org/grp2/gnode/GNodeServer.java
+++ gnode/src/main/java/org/grp2/gnode/GNodeServer.java
@@ -10,15 +10,7 @@ public class GNodeServer {
 
         int PORT = 7000;
 
-//        API api = new API(PORT);
-//        api.start();
-
-        GreenhouseController greenhouseController = new GreenhouseController(5000, "192.168.0.40");
-        greenhouseController.writeValue(Action.WRITE_FAN_SPEED, 1);
-        greenhouseController.writeValue(Action.WRITE_BLUE_LIGHT, 100);
-        System.out.println(greenhouseController.readValue(Action.READ_HUMIDITY));
-        System.out.println(greenhouseController.readValue(Action.READ_TEMPERATURE));
-        System.out.println(greenhouseController.readValue(Action.READ_BLUE_LIGHT));
-        System.out.println(greenhouseController.readValue(Action.READ_RED_LIGHT));
+        API api = new API(PORT);
+        api.start();
     }
 }
