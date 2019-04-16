1036625b5a669bd9d31e8a23a69134ff8287a578
Anders Bensen Ottsen <anders_bensen@hotmail.com>
2019-04-16 13:11:32 +0200

    Setup bootstrap

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GreenhouseManagementComponent } from "./greenhouse-management/greenhouse-management.component";
+import { ViewDataComponent } from "./view-data/view-data.component";
+import { SetpointsComponent } from "./setpoints/setpoints.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path: "greenhouse-management", component:GreenhouseManagementComponent},
+                        {path: "view-data", component:ViewDataComponent},
+                        {path: "setpoints", component:SetpointsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
