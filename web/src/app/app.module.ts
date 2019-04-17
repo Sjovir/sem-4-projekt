69881e442c9fd633e8ee39e1762f23e74ad7421d
Anders Bensen Ottsen <anders_bensen@hotmail.com>
2019-04-17 14:23:27 +0200

    Setup greenhouse
    Made regex and all except restcall for setup greenhouse

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { GreenhouseManagementComponent } from './greenhouse-management/greenhous
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/setup-greenhouse/setup-greenhouse.component.ts
+++ web/src/app/setup-greenhouse/setup-greenhouse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-setup-greenhouse',
@@ -7,9 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetupGreenhouseComponent implements OnInit {
 
-  constructor() { }
+  setupForm: FormGroup;
+  submitted = false;
+  success = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.setupForm = this.formBuilder.group({
+      ipAddress: ['', Validators.required],
+      port: ['', Validators.required],
+      location: ['', Validators.required],
+      name: ['', Validators.required]
+    })
+  }
 
   ngOnInit() {
   }
 
+  checkFields() {
+    var formIPAddress = this.setupForm.controls.ipAddress.value;
+    var validFormIPAddress = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(formIPAddress);
+
+    var formPort = this.setupForm.controls.port.value;
+    var validFormPort = /^[0-9]+$/.test(formPort);
+
+    var formLocation = this.setupForm.controls.location.value;
+    var validFormLocation = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/.test(formLocation);
+
+    var formName = this.setupForm.controls.name.value;
+    var validFormName = /^[0-9a-zA-Z]+$/.test(formName);
+
+    if(!(validFormIPAddress)) {
+      console.log(formIPAddress);
+      this.htmlErrorDiv = "Please enter a valid IP address.";
+      return false;
+    }
+
+    if(!(validFormPort && formPort.length == 4)) {
+      console.log(formPort);
+      this.htmlErrorDiv = "Your port must be 4 digits long.";
+      return false;
+    }
+
+    if(!(validFormLocation)) {
+      console.log(formLocation);
+      this.htmlErrorDiv = "Please enter a valid geolocation.";
+      return false;
+    }
+
+    if(!(validFormName && (formName.length >= 3 && formName.length <= 30))) {
+      console.log(formName);
+      this.htmlErrorDiv = "Your greenhouse name must be between 3 and 30 characters and/or numbers.";
+      return false;
+    }
+
+    this.doRestCall();
+    this.htmlErrorDiv = "You created an greenhouse.";
+    return true;
+  }
+
+  doRestCall() {
+    console.log("THIS IS A REST CALL");
+    return;
+  }
 }
