2e955a1a754fd5f2c77ef9dbdd6d3b8f86a60681
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2019-04-17 14:03:08 +0200

    completed mockup of view greenhousedata

--- web/src/greenhouse.ts
+++ web/src/greenhouse.ts
@@ -1,3 +1,11 @@
 export class Greenhouse{
-    
+    id: number;
+    name: string;
+    ipAddress: string;
+    port: number;
+    location: string;
+    dateCreated: string;
+    lightData: {timeCollected:string,redValue:number,blueValue:number}[];
+    humidityData: {timeCollected:string,value:number}[];
+    temperatureData:{timeCollected:string,value:number}[];
 }
