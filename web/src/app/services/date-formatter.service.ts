cafb668231effff9bafe806559b440edf3447fd5
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2019-05-01 11:26:16 +0200

    madcommit

--- web/src/app/services/date-formatter.service.ts
+++ web/src/app/services/date-formatter.service.ts
@@ -13,7 +13,15 @@ export class DateFormatterService {
     var date = new Date(epoch);
     var res:string="";
     res+=date.getUTCDate().toString()+"/"+date.getUTCMonth().toString()+"-"+date.getUTCFullYear().toString();
+    res+=" ";
+    res+=date.getUTCHours().toString()+":"+date.getUTCMinutes().toString()+":"+date.getUTCSeconds().toString();
 
     return res;
+  }
+  public addZero(i:number) :string {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i.toString();
   }
 }
