01a1a9041d7e7dc5e8dcfa0ffd89b4e40d7855b6
Anders Bensen Ottsen <anders_bensen@hotmail.com>
2019-05-02 09:40:34 +0200

    fix sldiers
    and values

--- web/src/app/temperature-setpoint/temperature-setpoint.component.ts
+++ web/src/app/temperature-setpoint/temperature-setpoint.component.ts
@@ -25,11 +25,11 @@ import { DataService } from '../services/data.service';
 
 export class TemperatureSetpointComponent implements OnInit {
 
-  public temperatureMin = 50; //get realtime values from database
-  public temperatureMax = 50; //get realtime values from database
+  public temperatureMin = 15; //get realtime values from database
+  public temperatureMax = 15; //get realtime values from database
   public temperatureAlarmMin = 50; //get realtime values from database
   public temperatureAlarmMax = 50; //get realtime values from database
-  
+
 
   constructor(private dataService: DataService) { }
 
